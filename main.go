c9469e447249679e446255d9a999a125fe0aaaea
Sherman Rose <shermanrose@improbable.io>
2022-08-07 06:18:19 +0100

    Simple server for heroku testing

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log"
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("hello world!")
+	var err error
+	defer func() {
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+	}()
+
+	mux := chi.NewRouter()
+	mux.Get("/status", handleStatus)
+
+	srv := &http.Server{
+		Addr:      ":8080",
+		Handler:   mux,
+		TLSConfig: nil,
+	}
+
+	err = srv.ListenAndServe()
 }
